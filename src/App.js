966c049703948bb9c4d2414c8c3fc66bf491d33b
Wes Kruger <wesleykruger@gmail.com>
2019-01-15 17:58:34 -0600

    All personal keys removed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import React, { Component } from 'react';
-import logo from './logo.png';
-import './App.css';
+import { ContentExplorer, ContentPicker, ContentPreview } from 'box-ui-elements';
+import messages from 'box-ui-elements/i18n/en-US';
+// import { addLocaleData } from 'react-intl';
+// import enLocaleData from 'react-intl/locale-data/en';
+// import logo from './logo.svg';
+import 'box-ui-elements/dist/explorer.css';
+import * as appAuth from './appauth.json';
+// import fs from "fs";
+// const appAuth = require('./appauth.json');
+// const appConfig = require('./config.js');
+// const BoxSDK = appConfig.boxSDK;
+const BoxSDK = require('box-node-sdk');  // Box SDK
+
 
 class App extends Component {
+	constructor(props) {
+		super(props);
+
+		const sdk = BoxSDK.getPreconfiguredInstance(appAuth);
+
+		var client = sdk.getAppAuthClient('enterprise');
+		console.log(client);
+
+		var token = '';
+		client.exchangeToken(['item_upload', 'item_delete'])
+			.then(tokenInfo => {
+				console.log("Hi I'm here")
+				token = tokenInfo;
+      });
+      
+      let devToken = "DEVTOKEN"
+	}
+
+
+  login() {
+    
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Welcome to Box!
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+    <ContentExplorer
+        token={this.devToken}
+        language='en-US'
+        messages={messages}
+    />
+
     );
   }
 }
